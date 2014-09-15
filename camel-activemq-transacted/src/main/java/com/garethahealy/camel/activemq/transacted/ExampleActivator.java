16962097c40c19eefa9b1b0956da0da9daef1e57
Gareth Healy <garethahealy@gmail.com>
2014-09-15 16:49:13 +0100

    Added example activator

---
+++ camel-activemq-transacted/src/main/java/com/garethahealy/camel/activemq/transacted/ExampleActivator.java
@@ -0,0 +1,18 @@
+package com.garethahealy.camel.activemq.transacted;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class ExampleActivator implements BundleActivator {
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        // TODO Auto-generated method stub
+    }
+}
