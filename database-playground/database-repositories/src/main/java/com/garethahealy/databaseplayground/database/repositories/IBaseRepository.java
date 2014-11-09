c4f60678391346147db7006854a15e70486ee267
Gareth Healy <garethahealy@gmail.com>
2014-11-09 18:28:50 +0000

    a

--- database-playground/database-repositories/src/main/java/com/garethahealy/databaseplayground/database/repositories/IBaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.boohoo.esb.database.osgiservices.repositories;
-
-import javax.persistence.EntityManager;
-
-public interface IBaseRepository {
-	
-	void setEntityManager(EntityManager entityManager);
-}
