03f6cda19edc2cf7cfcfeca81b250d7e69b6fb61
Gareth Healy <garethahealy@gmail.com>
2014-11-18 19:53:30 +0000

    Removed camelContext from blueprints as its not used in this file Updated test to provide a fake camel context

--- activemq-service/src/test/java/com/garethahealy/activemq/service/routes/CamelContextTest.java
+++ activemq-service/src/test/java/com/garethahealy/activemq/service/routes/CamelContextTest.java
@@ -1,5 +1,7 @@
 package com.garethahealy.activemq.service.routes;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,4 +17,12 @@ public class CamelContextTest extends CamelBlueprintTestSupport {
     public void camelContextIsNotNull() {
         Assert.assertNotNull(context);
     }
+    
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+    	//NOTE: This blueprints file doesnt have a camelContext, so we provide a fake one so the tests will run
+    	return new DefaultCamelContext(createRegistry());
+    }
+    
+    //TODO: set property 'activemq.url' to use vm
 }
