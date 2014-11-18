3d2598f9a8a9d0640a3407c9ba47e944c1cab6e6
Gareth Healy <garethahealy@gmail.com>
2014-11-18 21:16:44 +0000

    Removed dead code

--- camel-activemq-transacted/src/test/java/com/garethahealy/camel/activemq/transacted/routes/CamelContextTest.java
+++ camel-activemq-transacted/src/test/java/com/garethahealy/camel/activemq/transacted/routes/CamelContextTest.java
@@ -1,6 +1,5 @@
 package com.garethahealy.camel.activemq.transacted.routes;
 
-import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,16 +8,5 @@ public class CamelContextTest extends BaseCamelBlueprintTestSupport {
     @Test
     public void camelContextIsNotNull() {
         Assert.assertNotNull(context);
-    }
-    
-    public void test() throws Exception {
-    	context.getRouteDefinition("").adviceWith(context, new AdviceWithRouteBuilder() {
-			@Override
-			public void configure() throws Exception {
-				weaveByType(null);
-				defaultErrorHandler();
-				
-			}
-    	});
     }
 }
