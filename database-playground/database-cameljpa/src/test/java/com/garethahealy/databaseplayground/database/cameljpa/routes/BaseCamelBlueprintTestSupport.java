c4b896fef89ee2edcf80389607fb6e42c4ed25b1
Gareth Healy <garethahealy@gmail.com>
2014-11-26 20:15:08 +0000

    Add mocked osgi services so can start running tests

--- database-playground/database-cameljpa/src/test/java/com/garethahealy/databaseplayground/database/cameljpa/routes/BaseCamelBlueprintTestSupport.java
+++ database-playground/database-cameljpa/src/test/java/com/garethahealy/databaseplayground/database/cameljpa/routes/BaseCamelBlueprintTestSupport.java
@@ -3,7 +3,9 @@ package com.garethahealy.databaseplayground.database.cameljpa.routes;
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
 import org.apache.camel.util.KeyValueHolder;
 import org.mockito.Mockito;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Dictionary;
 import java.util.Map;
@@ -18,9 +20,11 @@ public class BaseCamelBlueprintTestSupport extends CamelBlueprintTestSupport {
         @SuppressWarnings("rawtypes")
         @Override
         protected void addServicesOnStartup(Map<String, KeyValueHolder<Object, Dictionary>> services) {
-                DataSource mysqlBasicManagedDataSource = Mockito.mock(DataSource.class);
+                PlatformTransactionManager platformTransactionManager = Mockito.mock(PlatformTransactionManager.class);
+                EntityManagerFactory entityManagerFactory = Mockito.mock(EntityManagerFactory.class);
 
-                services.put(DataSource.class.getCanonicalName(), asService(mysqlBasicManagedDataSource, "osgi.jndi.service.name", "jdbc/mysqlBasicManagedDataSource"));
+                services.put(PlatformTransactionManager.class.getCanonicalName(), asService(platformTransactionManager, null));
+                services.put(EntityManagerFactory.class.getCanonicalName(), asService(entityManagerFactory, "osgi.unit.name", "playground-persistence"));
         }
 }
 
