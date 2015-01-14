3631b41e0235077c61de6a2915783342b3a4c457
Gareth Healy <garethahealy@gmail.com>
2015-01-14 15:04:17 +0000

    Created very simple example of using JaxWsDynamicClientFactory

--- ws-security-https-cxf-client/src/main/java/com/garethahealy/wssecurity/https/cxf/client/services/WsHelloWorldService.java
+++ ws-security-https-cxf-client/src/main/java/com/garethahealy/wssecurity/https/cxf/client/services/WsHelloWorldService.java
@@ -20,20 +20,32 @@
 package com.garethahealy.wssecurity.https.cxf.client.services;
 
 import java.io.InvalidObjectException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import com.garethahealy.helloworld.HelloWorldEndpoint;
 import com.garethahealy.helloworld.HelloWorldRequest;
 import com.garethahealy.helloworld.HelloWorldResponse;
 import com.garethahealy.wssecurity.https.cxf.client.config.WsEndpointConfiguration;
+import com.garethahealy.wssecurity.https.cxf.client.config.WsMapper;
 import com.garethahealy.wssecurity.https.cxf.client.decorators.HTTPSWsSignatureEndpointDecorator;
 import com.garethahealy.wssecurity.https.cxf.client.javaxws.BareBones;
+import com.garethahealy.wssecurity.https.cxf.client.javaxws.DynamicClient;
 import com.garethahealy.wssecurity.https.cxf.client.resolvers.CachedResolver;
 import com.garethahealy.wssecurity.https.cxf.client.resolvers.Resolver;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.dynamic.DynamicClientFactory;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 
 public class WsHelloWorldService {
 
     private WsEndpointConfiguration<HelloWorldEndpoint> config;
     private boolean isCxfBeanFactory;
+
+    private String wsdl = "file:/Users/garethah/Documents/github/garethahealy/jboss-fuse-examples/ws-security-https-cxf-wsdl-helloworld/src/main/resources/wsdl/helloworld.wsdl";
 
     public WsHelloWorldService(WsEndpointConfiguration<HelloWorldEndpoint> config, boolean isCxfBeanFactory) {
         this.config = config;
@@ -53,5 +65,21 @@ public class WsHelloWorldService {
     public HelloWorldResponse sayHello(HelloWorldRequest request) throws InvalidObjectException {
         HelloWorldEndpoint endpoint = isCxfBeanFactory ? resolveEndpoint() : resolveByBareBones();
         return endpoint.sayHello(request);
+    }
+
+    public Object[] reflectionExample() throws Exception {
+        Map<String, Object> methodNameToValue = new HashMap<String, Object>();
+        methodNameToValue.put("setHello", "Hello? I am Mr Reflection!");
+
+        WsMapper mapper = new WsMapper();
+        mapper.setOperationName("SayHello");
+        mapper.setClassName(HelloWorldRequest.class.getCanonicalName());
+        mapper.setMethodNameToValue(methodNameToValue);
+
+        DynamicClient dynamicClient = DynamicClient.newInstance();
+        Client client = dynamicClient.createClient(wsdl);
+        Object request = dynamicClient.getRequest(mapper);
+
+        return dynamicClient.callMethod(client, mapper.getOperationName(), request);
     }
 }
