71874e07ab6fb22c7f2993bdea8f5743230996f0
Gareth Healy <garethahealy@gmail.com>
2015-02-07 12:20:28 +0000

    Added simple example of using camel-boon

---
+++ camel-boon-example/src/main/java/com/garethahealy/camelboon/PersonEntity.java
@@ -0,0 +1,52 @@
+/*
+ * #%L
+ * camel-boon-example
+ * %%
+ * Copyright (C) 2013 - 2015 Gareth Healy
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.garethahealy.camelboon;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class PersonEntity {
+
+    private String firstName;
+    private String surname;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("firstName", firstName)
+            .append("surname", surname)
+            .toString();
+    }
+}
